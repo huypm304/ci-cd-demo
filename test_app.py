326112b56ae157751ac45dfa4e5747e7cfe4ca55
Minh Huy <minhhuy3042004@gmai.com>
2025-10-29 19:19:29 +0700

    Update

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_app():
     client = app.test_client() 
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data.decode() == 'Hello World! ' 
+    assert response.data.decode() == 'Hello World! '
